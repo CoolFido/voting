5275b3053a7fb163f4e0947875b7a0e987e654d2
CoolFido <88465060+CoolFido@users.noreply.github.com>
2021-12-21 18:10:41 +0100

    Create Competition model

---
+++ app/Models/Competition.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Represents coding competition.
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Carbon\Carbon $from
+ * @property \Carbon\Carbon $to
+ *
+ * @property \Carbon\Carbon $created_at?
+ * @property \Carbon\Carbon $updated_at?
+ */
+class Competition extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'from', 'to'];
+    public $dates = ['from', 'to'];
+}
---
+++ database/migrations/2021_12_21_170524_create_competitions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompetitionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('competitions', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('from');
+            $table->date('to');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('competitions');
+    }
+}
