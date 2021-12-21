7289766a4e824045e0180bff318ae7b5453146b8
CoolFido <88465060+CoolFido@users.noreply.github.com>
2021-12-21 18:29:18 +0100

    Edit landing welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
+@section('title', 'Probíhající soutěže')
 @section('content')
-
 <div class="min-vh-100 d-flex justify-content-center align-items-center">
+
     @foreach(['success', 'danger'] as $type)
         @if(Session::has($type))
             <p class="alert bg-{{ $type }} text-light container">
@@ -10,15 +11,13 @@
             </p>
         @endif
     @endforeach
+
     <div class="card rounded-0 p-5 bg-dark shadow-lg text-secondary mx-sm-0 mx-5">
-        
         <h1>GalaxyCode voting system</h1>
-        <a href="" class="text-decoration-none">
+        <a href="{{ route('auth.login') }}" class="text-decoration-none">
             <h3 class="text-primary ">Log in</h3>
         </a>
-
     </div>
 
-</div> 
-
+</div>
 @endsection
